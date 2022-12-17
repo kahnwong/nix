f78bade1f9260d15687b120442cfb0735ea05eed
kahnwong <karn@karnwong.me>
2022-12-17 21:36:56 +0700

    fish: add envsource

--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@
   home.file.".config/fish/conf.d/work.fish".source = ./dotfiles/.config/fish/conf.d/work.fish;
   home.file.".config/fish/config.fish".source = ./dotfiles/.config/fish/config.fish;
   home.file.".config/fish/fish_plugins".source = ./dotfiles/.config/fish/fish_plugins;
+  home.file.".config/fish/functions/envsource.fish".source = ./dotfiles/.config/fish/functions/envsource.fish;
   home.file.".config/fish/functions/fish_prompt.fish".source = ./dotfiles/.config/fish/functions/fish_prompt.fish;
   home.file.".config/fish/functions/fuck.fish".source = ./dotfiles/.config/fish/functions/fuck.fish;
   home.file.".config/fish/functions/pip.fish".source = ./dotfiles/.config/fish/functions/pip.fish;
