abd6a3c9ee4448ae7b6ab9ca917306cd4eb5f1a4
kahnwong <karn@karnwong.me>
2023-01-29 23:46:47 +0700

    fix: update vscode config path for encryption/decryption

--- utils/decrypt_config.sh
+++ utils/decrypt_config.sh
@@ -8,5 +8,7 @@ sops -d .ssh/config.sops >.ssh/config
 sops -d .config/fish/conf.d/work.sops.fish >.config/fish/conf.d/work.fish
 sops -d .aws/config.sops >.aws/config
 sops -d .aws/credentials.sops >.aws/credentials
-sops -d settings.sops.json >settings.json
 sops -d .config/sops/age/keys.sops.txt >.config/sops/age/keys.txt
+
+cd ../config/vscode || exit
+sops -d settings.sops.json >settings.json
--- utils/encrypt_config.sh
+++ utils/encrypt_config.sh
@@ -8,5 +8,7 @@ sops -e .ssh/config >.ssh/config.sops
 sops -e .config/fish/conf.d/work.fish >.config/fish/conf.d/work.sops.fish
 sops -e .aws/config >.aws/config.sops
 sops -e .aws/credentials >.aws/credentials.sops
-sops -e settings.json >settings.sops.json
 sops -e .config/sops/age/keys.txt >.config/sops/age/keys.sops.txt
+
+cd ../config/vscode || exit
+sops -e settings.json >settings.sops.json
