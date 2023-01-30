20058805e93f8964f919936a34965dd69c3fbf6d
kahnwong <karn@karnwong.me>
2023-01-30 11:52:51 +0700

    sort files in utils

--- utils/decrypt_config.sh
+++ utils/decrypt_config.sh
@@ -3,12 +3,12 @@
 sops -d .enc.env >.env
 
 cd dotfiles || exit
-sops -d wakatime.sops.cfg >.wakatime.cfg
-sops -d .ssh/config.sops >.ssh/config
-sops -d .config/fish/conf.d/work.sops.fish >.config/fish/conf.d/work.fish
 sops -d .aws/config.sops >.aws/config
 sops -d .aws/credentials.sops >.aws/credentials
+sops -d .config/fish/conf.d/work.sops.fish >.config/fish/conf.d/work.fish
 sops -d .config/sops/age/keys.sops.txt >.config/sops/age/keys.txt
+sops -d .ssh/config.sops >.ssh/config
+sops -d wakatime.sops.cfg >.wakatime.cfg
 
 cd ../config/vscode || exit
 sops -d settings.sops.json >settings.json
--- utils/encrypt_config.sh
+++ utils/encrypt_config.sh
@@ -3,12 +3,12 @@
 sops -e .env >.enc.env
 
 cd dotfiles || exit
-sops -e .wakatime.cfg >wakatime.sops.cfg
-sops -e .ssh/config >.ssh/config.sops
-sops -e .config/fish/conf.d/work.fish >.config/fish/conf.d/work.sops.fish
 sops -e .aws/config >.aws/config.sops
 sops -e .aws/credentials >.aws/credentials.sops
+sops -e .config/fish/conf.d/work.fish >.config/fish/conf.d/work.sops.fish
 sops -e .config/sops/age/keys.txt >.config/sops/age/keys.sops.txt
+sops -e .ssh/config >.ssh/config.sops
+sops -e .wakatime.cfg >wakatime.sops.cfg
 
 cd ../config/vscode || exit
 sops -e settings.json >settings.sops.json
