72ec0769e7217be11fd4ddeb668566437160c696
kahnwong <karn@karnwong.me>
2023-04-19 12:20:58 +0700

    refactor: move git from home.nix to git.nix

--- home.nix
+++ home.nix
@@ -26,6 +26,11 @@
 
   manual.manpages.enable = false;
 
+  ### imports
+  imports = [
+    ./nix/git.nix
+  ];
+
   ### configurations
   home.file.".aws/config".source = ./dotfiles/.aws/config;
   home.file.".aws/credentials".source = ./dotfiles/.aws/credentials;
@@ -50,42 +55,7 @@
   home.file.".wakatime.cfg".source = ./dotfiles/.wakatime.cfg;
 
   ### git
-  programs.git = {
-    # `git config --global --edit` to see raw config
-    enable = true;
-    userName = builtins.getEnv "git_username";
-    userEmail = builtins.getEnv "git_email";
 
-    delta = {
-      enable = true;
-      options = {
-        navigate = true; # use n and N to move between diff sections
-        side-by-side = true;
-      };
-    };
-
-    extraConfig = {
-      diff.colorMoved = "default";
-      merge.conflictstyle = "diff3";
-      # url."git@github.com:".insteadOf = "https://github.com/";
-
-      ######################
-      # git_signing_key
-      ######################
-      user.signingkey = builtins.getEnv "user_signingkey";
-      gpg.format = "ssh";
-
-      commit = lib.mkIf pkgs.stdenv.isDarwin {
-        gpgsign = true;
-      };
-      credential = lib.mkIf pkgs.stdenv.isDarwin {
-        helper = "osxkeychain";
-      };
-      ######################
-      # end git_signing_key
-      ######################
-    };
-  };
 
   ### nvim
   programs.neovim = {
