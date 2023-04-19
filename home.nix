78396ac9bf3deca472908745621f50fdc8483b42
kahnwong <karn@karnwong.me>
2023-04-19 12:45:13 +0700

    move to nvim.nix

--- home.nix
+++ home.nix
@@ -30,6 +30,7 @@
   imports = [
     ./nix/fish.nix
     ./nix/git.nix
+    ./nix/nvim.nix
   ];
 
   ### configurations
@@ -44,59 +45,6 @@
   home.file.".topydo_columns".source = ./dotfiles/.topydo_columns;
   home.file.".topydo".source = ./dotfiles/.topydo;
   home.file.".wakatime.cfg".source = ./dotfiles/.wakatime.cfg;
-
-  ### nvim
-  programs.neovim = {
-    enable = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-
-    # default nix vim plugin manager is flaky with vim-wakatime, need to use vim-plug
-    plugins = with pkgs.vimPlugins; [
-      vim-plug
-    ];
-
-    extraConfig = ''
-      runtime! plug.vim
-      call plug#begin()
-
-      "diff
-      Plug 'mhinz/vim-signify'
-
-      "integrations
-      Plug 'wakatime/vim-wakatime'
-
-      "syntax highlighting
-      Plug 'chrisbra/csv.vim'
-      Plug 'jvirtanen/vim-hcl'
-      Plug 'LnL7/vim-nix'
-      Plug 'cweagans/vim-taskpaper'
-      "Plug 'preservim/vim-markdown'
-
-      "utilities
-      Plug 'windwp/nvim-autopairs'
-
-      Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
-      Plug 'junegunn/fzf.vim'
-
-      "ui
-      Plug 'stevearc/dressing.nvim'
-      Plug 'vim-airline/vim-airline'
-      Plug 'vim-airline/vim-airline-themes'
-
-      call plug#end()
-
-      """" config
-      set number
-      set expandtab
-
-      filetype plugin on
-
-      let g:airline_theme='onedark'
-      let g:airline_powerline_fonts = 1
-    '';
-  };
 
   home.packages = with pkgs;
     let
---
+++ nix/nvim.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, lib, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    # default nix vim plugin manager is flaky with vim-wakatime, need to use vim-plug
+    plugins = with pkgs.vimPlugins; [
+      vim-plug
+    ];
+
+    extraConfig = ''
+      runtime! plug.vim
+      call plug#begin()
+
+      "diff
+      Plug 'mhinz/vim-signify'
+
+      "integrations
+      Plug 'wakatime/vim-wakatime'
+
+      "syntax highlighting
+      Plug 'chrisbra/csv.vim'
+      Plug 'jvirtanen/vim-hcl'
+      Plug 'LnL7/vim-nix'
+      Plug 'cweagans/vim-taskpaper'
+      "Plug 'preservim/vim-markdown'
+
+      "utilities
+      Plug 'windwp/nvim-autopairs'
+
+      Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+      Plug 'junegunn/fzf.vim'
+
+      "ui
+      Plug 'stevearc/dressing.nvim'
+      Plug 'vim-airline/vim-airline'
+      Plug 'vim-airline/vim-airline-themes'
+
+      call plug#end()
+
+      """" config
+      set number
+      set expandtab
+
+      filetype plugin on
+
+      let g:airline_theme='onedark'
+      let g:airline_powerline_fonts = 1
+    '';
+  };
+}
