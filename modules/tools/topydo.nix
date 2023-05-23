52b01aa5e61c09f228dcf89339c97483011ffc4c
kahnwong <karn@karnwong.me>
2023-05-23 12:58:12 +0000

    todotxt: replace path via nix

--- modules/tools/topydo.nix
+++ modules/tools/topydo.nix
@@ -2,7 +2,13 @@
 
 {
   home.file.".topydo_columns".source = ./topydo/.topydo_columns;
-  home.file.".topydo".source = ./topydo/.topydo;
+  # home.file.".topydo".source = ./topydo/.topydo;
+
+  home.file = {
+    ".topydo" = {
+      text = builtins.replaceStrings [ "ARCHIVE_FILENAME" "TODO_FILENAME" ] [ "/opt/syncthing/cloud/Apps/todotxt/done.txt" "/opt/syncthing/cloud/Apps/todotxt/todo.txt" ] (builtins.readFile ./topydo/.topydo);
+    };
+  };
 
   home.packages = with pkgs;
     let
