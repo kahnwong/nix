2b49f9287c384820c5b940dfc468319d0ca1b204
kahnwong <karn@karnwong.me>
2023-09-15 19:09:17 +0700

    add brew autocomplete

--- scripts/common/03_add_fish_completions.sh
+++ scripts/common/03_add_fish_completions.sh
@@ -12,3 +12,6 @@ rm -rf temp
 
 ## docker
 wget --backups 0 -P ~/.config/fish/completions https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish
+
+## brew
+wget --backups 0 -P ~/.config/fish/completions https://raw.githubusercontent.com/Homebrew/brew/master/completions/fish/brew.fish
