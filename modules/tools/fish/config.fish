8ee24533c5fa77f89c9de92f43fef11edb8685bc
kahnwong <karn@karnwong.me>
2023-09-24 16:15:55 +0700

    add intellij path

--- modules/tools/fish/config.fish
+++ modules/tools/fish/config.fish
@@ -17,6 +17,7 @@ set PATH \
     $HOME/.local/bin \
     $HOME/.npm-global/bin \
     $HOME/.krew/bin \
+    /Applications/IntelliJ\ IDEA.app/Contents/MacOS \
     /opt/homebrew/bin \
     /usr/local/bin \
     /usr/local/sbin \
