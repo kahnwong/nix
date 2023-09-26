2d944b44284438195d9dce0fe7dee162c9ad059e
kahnwong <karn@karnwong.me>
2023-09-26 21:09:12 +0700

    update ssh key

--- modules/tools/ssh.nix
+++ modules/tools/ssh.nix
@@ -2,4 +2,8 @@
 
 {
   home.file.".ssh/config".source = ./ssh/config;
+  home.file.".ssh/config.d/homelab".source = ./ssh/config.d/homelab;
+  home.file.".ssh/config.d/vps".source = ./ssh/config.d/vps;
+  home.file.".ssh/config.d/work".source = ./ssh/config.d/work;
+  home.file.".ssh/config.d/work-legacy".source = ./ssh/config.d/work-legacy;
 }
