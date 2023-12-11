60f3a602c9771737887597a6b66392b79b5c1f1f
kahnwong <karn@karnwong.me>
2023-12-11 11:01:59 +0700

    add nvim plugins via nvchad

--- scripts/common/03_nvim.sh
+++ scripts/common/03_nvim.sh
@@ -5,3 +5,4 @@ git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 nvim
 cp "$(pwd)/scripts/common/nvchad/overrides.lua" ~/.config/nvim/lua/custom/configs/overrides.lua
 cp "$(pwd)/scripts/common/nvchad/chadrc.lua" ~/.config/nvim/lua/custom/chadrc.lua
+cp "$(pwd)/scripts/common/nvchad/plugins.lua" ~/.config/nvim/lua/custom/plugins.lua
