0e3bc35be477830249ba172d23e7bf870990b721
Karn Wong <karn@karnwong.me>
2024-01-14 11:48:44 +0700

    update nvchad setup

--- scripts/common/03_nvim.sh
+++ scripts/common/03_nvim.sh
@@ -4,9 +4,6 @@ git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
 nvim
 mkdir -p ~/.config/nvim/lua/custom/configs
-cp "$(pwd)/scripts/common/nvchad/overrides.lua" ~/.config/nvim/lua/custom/configs/overrides.lua
-cp "$(pwd)/scripts/common/nvchad/lspconfig.lua" ~/.config/nvim/lua/custom/configs/lspconfig.lua
-cp "$(pwd)/scripts/common/nvchad/chadrc.lua" ~/.config/nvim/lua/custom/chadrc.lua
-cp "$(pwd)/scripts/common/nvchad/plugins.lua" ~/.config/nvim/lua/custom/plugins.lua
-cp "$(pwd)/scripts/common/nvchad/highlights.lua" ~/.config/nvim/lua/custom/highlights.lua
-cp "$(pwd)/scripts/common/nvchad/mappings.lua" ~/.config/nvim/lua/custom/mappings.lua
+
+rm -rf ~/.config/nvim/lua/custom
+cp -r "$(pwd)/scripts/common/nvchad/custom" ~/.config/nvim/lua/
