63623f78c909207f1fcf4f9ede859239a585d620
Karn Wong <karn@karnwong.me>
2024-02-13 20:42:33 +0700

    add thunderbird

--- scripts/linux-desktop/02_install_linux.sh
+++ scripts/linux-desktop/02_install_linux.sh
@@ -17,3 +17,4 @@ flatpak install flathub com.github.eneshecan.WhatsAppForLinux -y
 flatpak install flathub com.google.Chrome -y
 flatpak install flathub io.mpv.Mpv -y
 flatpak install flathub org.mozilla.firefox -y
+flatpak install flathub org.mozilla.Thunderbird -y
