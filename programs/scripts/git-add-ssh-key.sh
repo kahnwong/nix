97fb7621af904ad3baa6560017f4a84671d98e33
Karn Wong <karn@karnwong.me>
2024-02-28 09:41:59 +0700

    rename ssh keys

--- programs/scripts/git-add-ssh-key.sh
+++ programs/scripts/git-add-ssh-key.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh-add --apple-use-keychain ~/.ssh/github_signing_key
-ssh-add --apple-use-keychain ~/.ssh/forgejo_signing_key
+ssh-add --apple-use-keychain ~/.ssh/github_signing_key.pem
+ssh-add --apple-use-keychain ~/.ssh/forgejo_signing_key.pem
