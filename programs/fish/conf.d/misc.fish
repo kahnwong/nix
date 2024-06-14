1fb6479d88b16a22da78b975cfd4b48b6a5fca58
Karn Wong <karn@karnwong.me>
2024-06-14 16:47:08 +0700

    add calendar function

--- programs/fish/conf.d/misc.fish
+++ programs/fish/conf.d/misc.fish
@@ -6,6 +6,21 @@ function tz # <https://github.com/oz/tz>
     TZ_LIST="US/Eastern,USA - New York;US/Pacific,USA - Seattle;Europe/Riga,LV - Riga;Europe/London,UK - London" command tz
 end
 
+function c
+    icalBuddy \
+      -ic A3340F36-8323-445F-BE7E-E0DC73FF26FD,140DA448-1B0A-42E0-85FE-DCF3949A44E9,CAB7A17B-8F21-4A64-A73B-E2756218CDB3 \
+      -f \
+      -npn \
+      -nc \
+      -iep "title,datetime" \
+      -ps "| - |" \
+      -po "datetime,title" \
+      -df "%H:%M" \
+      -eed \
+      -n \
+      eventsToday
+end
+
 # function budget
 #     fava -p 5004 ~/Cloud/Apps/fava/beans.beancount
 # end
