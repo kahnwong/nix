25b5bcc2dfe48c5240c4500565ded63c0b3fb82a
Karn Wong <karn@karnwong.me>
2024-07-02 14:23:00 +0000

    conditional timesheet config

--- programs/timesheet/timesheet.nix
+++ programs/timesheet/timesheet.nix
@@ -1,7 +1,16 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.file.".config/timesheet/config.ini".source = ./config.ini;
+  home.file = {
+    ".config/timesheet/config.ini" = {
+      text = with pkgs;
+        let
+          mac = builtins.replaceStrings [ "FILENAME" ] [ "/Users/kahnwong/Cloud/Apps/timesheet/timesheet.txt" ] (builtins.readFile ./config.ini);
+          linux = builtins.replaceStrings [ "FILENAME" ] [ "/opt/syncthing/cloud/Apps/timesheet/timesheet.txt" ] (builtins.readFile ./config.ini);
+        in
+        (if stdenv.isLinux then linux else mac);
+    };
+  };
 
   home.packages = with pkgs;
     [
