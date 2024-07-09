19224764ed5ece98bcd706318ad44dfe0b1fcffb
Karn Wong <karn@karnwong.me>
2024-07-09 21:12:25 +0700

    fix ssh-agent on linux

--- programs/scripts/git-add-ssh-key.sh
+++ programs/scripts/git-add-ssh-key.sh
@@ -5,6 +5,7 @@ if [[ $(uname -s) == 'Darwin' ]]; then
 	ssh-add --apple-use-keychain ~/.ssh/forgejo_signing_key.pem
 
 elif [[ $(uname -s) == 'Linux' ]]; then
+	ssh-agent fish
 	ssh-add ~/.ssh/github_signing_key.pem
 	ssh-add ~/.ssh/forgejo_signing_key.pem
 fi
