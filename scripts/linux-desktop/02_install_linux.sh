838ab1472e06babaaa123378646fc695283a42c6
Karn Wong <karn@karnwong.me>
2024-07-21 23:15:45 +0700

    flatpak: remove unused apps

--- scripts/linux-desktop/02_install_linux.sh
+++ scripts/linux-desktop/02_install_linux.sh
@@ -16,20 +16,20 @@ sudo snap install intellij-idea-ultimate --classic
 sudo apt install flameshot
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
+# flatpak install flathub com.github.eneshecan.WhatsAppForLinux -y
+# flatpak install flathub com.sindresorhus.Caprine -y
 # flatpak install flathub com.usebottles.bottles -y
+# flatpak install flathub org.kde.konsole -y
 # flatpak install flathub org.mozilla.firefox -y  # already installed via snap by default
 flatpak install flathub com.calibre_ebook.calibre -y
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y
-flatpak install flathub com.github.eneshecan.WhatsAppForLinux -y
 flatpak install flathub com.github.hluk.copyq -y
 flatpak install flathub com.github.johnfactotum.Foliate -y
 flatpak install flathub com.github.zocker_160.SyncThingy -y
 flatpak install flathub com.google.Chrome -y
-flatpak install flathub com.sindresorhus.Caprine -y
 flatpak install flathub dev.k8slens.OpenLens -y
 flatpak install flathub io.github.dweymouth.supersonic -y
 flatpak install flathub io.mpv.Mpv -y
 flatpak install flathub org.fkoehler.KTailctl -y
-flatpak install flathub org.kde.konsole -y
 flatpak install flathub org.mozilla.Thunderbird -y
