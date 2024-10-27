c9d9099b3f363519cc1cf83dd303421b2ab28c45
Karn Wong <karn@karnwong.me>
2024-10-27 14:19:38 +0700

    config: add comments

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# aws
 mkdir -p ~/.aws
 sops -d ./programs/aws/config.sops >~/.aws/config
 sops -d ./programs/aws/credentials.sops >~/.aws/credentials
 
+# fish
 sops -d ./programs/fish/conf.d/work.sops.fish >~/.config/fish/conf.d/work.fish
 
+# k8s
 mkdir -p ~/.kube
 sops -d ./programs/kubectl/config.sops >~/.kube/config
 
+# ssh
 mkdir -p ~/.ssh/config.d
 sops -d ./programs/ssh/config.d/homelab.sops >~/.ssh/config.d/homelab
 sops -d ./programs/ssh/config.d/vps.sops >~/.ssh/config.d/vps
@@ -16,8 +20,12 @@ sops -d ./programs/ssh/config.d/vps.sops >~/.ssh/config.d/vps
 # sops -d ./programs/ssh/config.d/work.sops >~/.ssh/config.d/work
 sops -d ./programs/ssh/config.sops >~/.ssh/config
 
+# timesheet
 mkdir -p ~/.config/timesheet
 sops -d ./programs/timesheet/config.sops.ini.txt >~/.config/timesheet/config.ini
+
+# wakatime
 sops -d ./programs/wakatime/wakatime.sops.cfg >~/.wakatime.cfg
 
-sops -d ./scripts/vscode/config/settings.sops.json >./scripts/vscode/config/settings.json
+## vscode
+#sops -d ./scripts/vscode/config/settings.sops.json >./scripts/vscode/config/settings.json
