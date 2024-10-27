1e184c907e3a97d739ce4ed4f04e736c45c95cda
Karn Wong <karn@karnwong.me>
2024-10-27 14:18:42 +0700

    git: rename signing key for gitea

--- programs/scripts/git-add-ssh-key.sh
+++ programs/scripts/git-add-ssh-key.sh
@@ -2,10 +2,10 @@
 
 if [[ $(uname -s) == 'Darwin' ]]; then
 	ssh-add --apple-use-keychain ~/.ssh/github_signing_key.pem
-	ssh-add --apple-use-keychain ~/.ssh/forgejo_signing_key.pem
+	ssh-add --apple-use-keychain ~/.ssh/gitea_signing_key.pem
 
 elif [[ $(uname -s) == 'Linux' ]]; then
 	# ssh-agent fish # only required if sometimes ssh version borks
 	ssh-add ~/.ssh/github_signing_key.pem
-	ssh-add ~/.ssh/forgejo_signing_key.pem
+	ssh-add ~/.ssh/gitea_signing_key.pem
 fi
