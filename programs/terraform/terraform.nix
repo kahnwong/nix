b787309ad6b10fca5fbd65e46ecfe1f4263f9542
Karn Wong <karn@karnwong.me>
2024-12-29 01:32:15 +0700

    terraform: add provider overrides

--- programs/terraform/terraform.nix
+++ programs/terraform/terraform.nix
@@ -1,7 +1,20 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.file.".terraformrc".source = ./.terraformrc;
+  #  home.file.".terraformrc".source = ./.terraformrc;
+  home.file = {
+    ".terraformrc" = {
+      text = with pkgs;
+        let
+          mac = builtins.replaceStrings [ "GOBIN" ] [ "/Users/kahnwong/go/bin" ]
+            (builtins.readFile ./.terraformrc);
+          linux =
+            builtins.replaceStrings [ "GOBIN" ] [ "/home/kahnwong/go/bin" ]
+            (builtins.readFile ./.terraformrc);
+        in (if stdenv.isLinux then linux else mac);
+    };
+  };
+
   home.file.".terraform.d/plugin-cache/.terraformrc".source =
     ./.terraformrc; # dummy file for init directory
 
