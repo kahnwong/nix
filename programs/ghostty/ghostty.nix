d4591549f0b65a45f415d6e7dc27c5fbfb75ac33
Karn Wong <karn@karnwong.me>
2024-12-30 20:35:39 +0700

    ghostty: amend font size on mac

--- programs/ghostty/ghostty.nix
+++ programs/ghostty/ghostty.nix
@@ -5,12 +5,14 @@
     ".config/ghostty/config" = {
       text = with pkgs;
         let
-          mac = builtins.replaceStrings [ "FISH_PATH" "FONT_SIZE"]
-            [ "/Users/kahnwong/.nix-profile/bin/fish" "27" ]
-            (builtins.readFile ./config);
-          linux = builtins.replaceStrings [ "FISH_PATH" "FONT_SIZE"]
-            [ "/home/kahnwong/.nix-profile/bin/fish" "10" ]
-            (builtins.readFile ./config);
+          mac = builtins.replaceStrings [ "FISH_PATH" "FONT_SIZE" ] [
+            "/Users/kahnwong/.nix-profile/bin/fish"
+            "13.5"
+          ] (builtins.readFile ./config);
+          linux = builtins.replaceStrings [ "FISH_PATH" "FONT_SIZE" ] [
+            "/home/kahnwong/.nix-profile/bin/fish"
+            "10"
+          ] (builtins.readFile ./config);
         in (if stdenv.isLinux then linux else mac);
     };
   };
