787a33da9c4eb119453ea9a2fc340b86d8d67e3f
Karn Wong <karn@karnwong.me>
2025-01-17 15:41:50 +0700

    flatpak: update apps

--- scripts/linux-desktop/02_install_linux.sh
+++ scripts/linux-desktop/02_install_linux.sh
@@ -15,39 +15,42 @@
 sudo apt install file-roller -y
 sudo apt install dolphin -y
 sudo apt install xclip -y
-sudo snap install czkawka
 sudo snap install intellij-idea-ultimate --classic
 sudo snap install mpv
-sudo snap install obsidian --classic
 
-# flatpak install flathub com.github.eneshecan.WhatsAppForLinux -y
-# flatpak install flathub com.sindresorhus.Caprine -y
 # flatpak install flathub com.usebottles.bottles -y
 # flatpak install flathub dev.skynomads.Seabird
 # flatpak install flathub io.mpv.Mpv -y
-# flatpak install flathub org.kde.konsole -y
 # flatpak install flathub org.mozilla.firefox -y  # already installed via snap by default
 flatpak install flathub com.calibre_ebook.calibre -y
 flatpak install flathub com.discordapp.Discord -y
 flatpak install flathub com.dropbox.Client -y
 flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y
+flatpak install flathub com.github.eneshecan.WhatsAppForLinux -y
 flatpak install flathub com.github.hluk.copyq -y
 flatpak install flathub com.github.iwalton3.jellyfin-media-player -y
 flatpak install flathub com.github.jeromerobert.pdfarranger -y
 flatpak install flathub com.github.johnfactotum.Foliate -y
+flatpak install flathub com.github.qarmin.czkawka -y
 flatpak install flathub com.github.ransome1.sleek -y
+flatpak install flathub com.github.tchx84.Flatseal -y
 flatpak install flathub com.github.zocker_160.SyncThingy -y
 flatpak install flathub com.google.Chrome -y
+flatpak install flathub com.heroicgameslauncher.hgl -y
+flatpak install flathub com.sindresorhus.Caprine -y
 flatpak install flathub com.transmissionbt.Transmission -y
 flatpak install flathub dev.k8slens.OpenLens -y
 flatpak install flathub io.github.TransmissionRemoteGtk -y
+flatpak install flathub io.github.ciromattia.kcc -y
 flatpak install flathub io.github.dweymouth.supersonic -y
 flatpak install flathub io.github.flattool.Warehouse -y
+flatpak install flathub io.github.sigmasd.stimulator -y
 flatpak install flathub io.gitlab.adhami3310.Impression -y
 flatpak install flathub io.kinvolk.Headlamp -y
+flatpak install flathub md.obsidian.Obsidian -y
 flatpak install flathub org.audacityteam.Audacity -y
 flatpak install flathub org.fkoehler.KTailctl -y
 flatpak install flathub org.flameshot.Flameshot -y
+flatpak install flathub org.kde.konsole -y
 flatpak install flathub org.kde.okular -y
 flatpak install flathub org.mozilla.Thunderbird -y
-flatpak install flathub io.github.sigmasd.stimulator -y
