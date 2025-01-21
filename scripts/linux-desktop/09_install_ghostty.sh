580e8030f57898edbe9fbd1937e12d6217a4ad85
Karn Wong <karn@karnwong.me>
2025-01-21 10:12:29 +0700

    ghostty: silence welcome message for fish shell on mac

--- scripts/linux-desktop/09_install_ghostty.sh
+++ scripts/linux-desktop/09_install_ghostty.sh
@@ -12,3 +12,9 @@ GHOSTTY_DEB_FILE=$(basename "$GHOSTTY_DEB_URL")
 curl -LO "$GHOSTTY_DEB_URL"
 sudo dpkg -i "$GHOSTTY_DEB_FILE"
 rm "$GHOSTTY_DEB_FILE"
+
+# disable welcome message for fish shell on mac
+# <https://stackoverflow.com/questions/15769615/remove-last-login-message-for-new-tabs-in-terminal>
+if [[ $(uname -s) == 'Darwin' ]]; then
+	touch ~/.hushlogin
+fi
