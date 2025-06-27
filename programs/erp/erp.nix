8c46b35b41bb31faff1086a66ad8689ab746c512
Karn Wong <karn@karnwong.me>
2025-06-27 12:14:44 +0700

    erp: make it work with darwin

--- programs/erp/erp.nix
+++ programs/erp/erp.nix
@@ -1,7 +1,20 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.file.".config/erp/config.yaml".source = ./config.yaml;
+
+  home.file = {
+    ".config/erp/config.yaml" = {
+      text = with pkgs;
+        let
+          mac = builtins.replaceStrings [ "PATH" ]
+            [ "/Users/kahnwong/Cloud/Apps/erp/erp.txt" ]
+            (builtins.readFile ./config.yaml);
+          linux = builtins.replaceStrings [ "PATH" ]
+            [ "/home/kahnwong/Cloud/Apps/erp/erp.txt" ]
+            (builtins.readFile ./config.yaml);
+        in (if stdenv.isLinux then linux else mac);
+    };
+  };
 
   home.packages = with pkgs; [ ];
 }
