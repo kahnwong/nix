afd7a7f3d77ac218a1430dca8fed057bedd7b85b
Karn Wong <karn@karnwong.me>
2025-07-23 21:00:19 +0700

    media-discord-rpc: update config

--- hosts/common-dev.nix
+++ hosts/common-dev.nix
@@ -1,22 +1,23 @@
 { pkgs, ... }: {
   imports = [
     ../programs/aws/aws.nix
-    ../programs/erp/erp.nix
     ../programs/ghostty/ghostty.nix
     ../programs/git/git.nix
     # ../programs/kitty/kitty.nix
-    ../programs/pgconn/pgconn.nix
-    ../programs/proxmox/proxmox.nix
     ../programs/scripts/scripts.nix
     ../programs/terraform/terraform.nix
-    ../programs/umami/umami.nix
     ../programs/yt-dlp/yt-dlp.nix
 
     # apps
     ../programs/article-summarizer/article-summarizer.nix
     ../programs/book-summarizer/book-summarizer.nix
+    ../programs/erp/erp.nix
+    ../programs/media-discord-rpc/media-discord-rpc.nix
+    ../programs/pgconn/pgconn.nix
+    ../programs/proxmox/proxmox.nix
     ../programs/topydo/topydo.nix
     ../programs/totp/totp.nix
+    ../programs/umami/umami.nix
     ../programs/wallabag-tagger/wallabag-tagger.nix
   ];
 
---
+++ programs/media-discord-rpc/media-discord-rpc.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home.file.".config/media-discord-rpc/config.sops.yaml".source = ./config.sops.yaml;
+
+  home.packages = with pkgs; [ ];
+}
