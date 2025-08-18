636faf11e509a1baf5bce756fd9195cd87004251
Karn Wong <karn@karnwong.me>
2025-08-18 12:07:51 +0700

    zed: sync config for flatpak

--- programs/zed/zed.nix
+++ programs/zed/zed.nix
@@ -1,7 +1,9 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.file.".config/zed/settings.json".source = ./settings.json;
+  home.file.".config/zed/settings.json".source = ./settings.json; # darwin
+  home.file.".var/app/dev.zed.Zed/config/zed/settings.json".source =
+    ./settings.json; # flatpak
 
   home.packages = with pkgs; [ ];
 }
