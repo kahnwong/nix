4175adf9e9e28e4ddbdc4c9f857c244e006075c3
Karn Wong <karn@karnwong.me>
2025-08-22 11:45:30 +0700

    mac: prune apps

--- scripts/mac/02_install_mac.sh
+++ scripts/mac/02_install_mac.sh
@@ -6,40 +6,39 @@ echo "================================= Install brew apps ======================
 brew install --cask firefox
 brew install --cask firefox-nightly
 brew install --cask google-chrome
-brew install --cask microsoft-edge
+# brew install --cask microsoft-edge
 
 # cli
 brew install mas
 brew install mpv
 
 # cloud storage
-brew install --cask dropbox
+# brew install --cask dropbox
 brew install --cask syncthing
 
 # communication
-brew install --cask adium
+# brew install --cask adium
+# brew install --cask whatsapp
 brew install --cask discord
 brew install --cask element
 brew install --cask messenger
 brew install --cask microsoft-teams
 brew install --cask slack
 brew install --cask thunderbird
-brew install --cask whatsapp
 brew install --cask zoom
 
 # data
-# brew install --cask qgis
 brew install --cask tad
 brew install apache-spark
 
 # dev tools
-# brew install --cask mongodb-compass
 # brew install --cask devtoys
 # brew install --cask docker
+# brew install --cask microsoft-remote-desktop
+# brew install --cask mongodb-compass
 brew install --cask fork
 brew install --cask ghostty
 brew install --cask intellij-idea
-brew install --cask microsoft-remote-desktop
 brew install --cask tailscale
 brew install --cask zed
 
@@ -78,9 +77,12 @@ brew install lihaoyun6/tap/quickrecorder
 
 # utilities
 # brew install --cask hiddenbar
+# brew install --cask kap
 # brew install --cask protonvpn
 # brew install --cask spectacle
 # brew install --cask stats
+# brew install ical-buddy
+#brew install --cask michaelvillar-timer
 brew install --cask alfred
 brew install --cask angry-ip-scanner
 brew install --cask anydesk
@@ -91,10 +93,8 @@ brew install --cask cheatsheet
 brew install --cask coconutbattery
 brew install --cask flameshot
 brew install --cask hot
-# brew install --cask kap
 brew install --cask keepingyouawake
 brew install --cask maccy
-#brew install --cask michaelvillar-timer
 brew install --cask mullvadvpn
 brew install --cask raspberry-pi-imager
 brew install --cask shifty
@@ -102,7 +102,6 @@ brew install --cask the-unarchiver
 brew install --cask tyke
 brew install --cask yubico-authenticator
 brew install --cask yubico-yubikey-manager
-brew install ical-buddy
 brew install jordanbaird-ice
 
 # peripheral software
@@ -115,4 +114,3 @@ mas install 1176895641 # spark
 # mas install 1423210932 # flow - focus & pomodoro timer
 mas install 1451685025 # wireguard
 mas install 539883307  # LINE
-mas install 568494494  # pocket
