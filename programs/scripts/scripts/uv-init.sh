3952955e6975c29a7da9c02fef93725a297e204f
Karn Wong <karn@karnwong.me>
2025-08-27 17:45:03 +0700

    uv: init as package by default

--- programs/scripts/scripts/uv-init.sh
+++ programs/scripts/scripts/uv-init.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
 uv python pin 3.13
-uv init --author-from git
+uv init --package --author-from git --no-description
 uv venv
-
-cat <<EOF >>pyproject.toml
-[tool.hatch.build.targets.wheel]
-packages = ["src"]
-
-[build-system]
-requires = ["hatchling"]
-build-backend = "hatchling.build"
-EOF
-uv pip install -e .
-
-rm hello.py
