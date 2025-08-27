e0c1d6ad6c2914810194c6b013d1a28ae5f3bd4b
Karn Wong <karn@karnwong.me>
2025-08-27 17:32:42 +0700

    Revert "uv: update init script"
    This reverts commit 6b914b2c3b663756c9d95820efff60cdf371a6c2.

--- programs/scripts/scripts/uv-init.sh
+++ programs/scripts/scripts/uv-init.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 uv python pin 3.13
-uv init --author-from git --no-description
+uv init --author-from git
 uv venv
 
-# -- seems like we no longer need these in 2025-08
-#cat <<EOF >>pyproject.toml
-#[tool.hatch.build.targets.wheel]
-#packages = ["src"]
-#
-#[build-system]
-#requires = ["hatchling"]
-#build-backend = "hatchling.build"
-#EOF
-#uv pip install -e .
+cat <<EOF >>pyproject.toml
+[tool.hatch.build.targets.wheel]
+packages = ["src"]
 
-rm main.py
+[build-system]
+requires = ["hatchling"]
+build-backend = "hatchling.build"
+EOF
+uv pip install -e .
+
+rm hello.py
