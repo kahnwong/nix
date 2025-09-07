142a0473a613483e90841e5a1f59e780eb7666c4
Karn Wong <karn@karnwong.me>
2025-09-07 11:24:12 +0700

    setup: add uv tool upgrade

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,16 +9,13 @@ nix-store --gc
 # fish
 fisher update
 ./scripts/common/02_add_fish_completions.sh
+rm ~/.config/fish/completions/git.fish # it borks git completion
 
 # gh extensions
 gh ext upgrade --all
 
-# brew
-if [[ $(uname -s) == 'Darwin' ]]; then
-	brew upgrade --greedy
-	brew autoremove
-	brew cleanup
-fi
+# python
+uv tool upgrade --all
 
 # rust
 rustup update
@@ -27,4 +24,11 @@ rustup update
 if [[ $(uname -s) == 'Linux' ]]; then
 	flatpak update
 	flatpak uninstall --unused
+fi
+
+# brew
+if [[ $(uname -s) == 'Darwin' ]]; then
+	brew upgrade --greedy
+	brew autoremove
+	brew cleanup
 fi
