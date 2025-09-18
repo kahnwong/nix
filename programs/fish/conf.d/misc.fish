de1d8b78689ef71a671af2d43b65ba9457ed5ad6
Karn Wong <karn@karnwong.me>
2025-09-18 09:57:29 +0700

    fish: update browsh for linux

--- programs/fish/conf.d/misc.fish
+++ programs/fish/conf.d/misc.fish
@@ -6,11 +6,10 @@ function tz # <https://github.com/oz/tz>
     TZ_LIST="US/Eastern,USA - New York;US/Pacific,USA - Seattle;Europe/Amsterdam,NL - Amsterdam;Europe/Riga,LV - Riga;Europe/London,UK - London" command tz
 end
 
-function browsh
-    set home_path "/home/kahnwong"
-    if test (uname) = "Darwin"
-        set home_path "/Users/kahnwong"
-    end
 
-    docker run -it --rm -v $home_path/.config/browsh/config.toml:/app/.config/browsh/config.toml browsh/browsh
+switch (uname)
+    case Linux
+        function browsh
+            docker run -it --rm -v "$HOME/.config/browsh/config.toml:/app/.config/browsh/config.toml" browsh/browsh
+        end
 end
