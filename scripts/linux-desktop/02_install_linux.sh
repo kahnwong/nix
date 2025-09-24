0d19f6c94b76b78f13b67749625deb949f8617dc
Karn Wong <karn@karnwong.me>
2025-09-24 20:00:27 +0700

    flatpak: prune

--- scripts/linux-desktop/02_install_linux.sh
+++ scripts/linux-desktop/02_install_linux.sh
@@ -4,16 +4,11 @@
 sudo apt install dolphin -y
 sudo apt install file-roller -y
 sudo apt install xclip -y
-sudo snap install intellij-idea-ultimate --classic
+# sudo snap install intellij-idea-ultimate --classic
 sudo snap install mpv
-sudo snap install notion-calendar-snap
 
-# flatpak install flathub com.sindresorhus.Caprine -y
-# flatpak install flathub com.usebottles.bottles -y
-# flatpak install flathub dev.skynomads.Seabird
 # flatpak install flathub io.mpv.Mpv -y
 # flatpak install flathub org.mozilla.firefox -y  # already installed via snap by default
-# flatpak install flathub be.alexandervanhee.gradia -y
 flatpak install flathub app.freelens.Freelens -y
 flatpak install flathub com.calibre_ebook.calibre -y
 flatpak install flathub com.discordapp.Discord -y
@@ -30,8 +25,6 @@ flatpak install flathub com.github.tchx84.Flatseal -y
 flatpak install flathub com.github.zocker_160.SyncThingy -y
 flatpak install flathub com.google.Chrome -y
 flatpak install flathub com.heroicgameslauncher.hgl -y
-flatpak install flathub com.ktechpit.whatsie -y
-flatpak install flathub com.microsoft.Edge -y
 flatpak install flathub com.slack.Slack -y
 flatpak install flathub com.todoist.Todoist -y
 flatpak install flathub com.transmissionbt.Transmission -y
