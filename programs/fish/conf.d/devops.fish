7539ced249ed96f8da9b28bd656674216a48de89
Karn Wong <karn@karnwong.me>
2025-09-25 17:43:52 +0700

    fish: add security alias

--- programs/fish/conf.d/devops.fish
+++ programs/fish/conf.d/devops.fish
@@ -8,6 +8,15 @@ function ntfy
     curl -d "Task completed" $(get_fish_secret NTFY_TOPIC)
 end
 
+# security
+function sbom
+    syft scan dir:.
+end
+
+function vuln
+    grype dir:.
+end
+
 # terraform
 function tf
   tofu $argv
