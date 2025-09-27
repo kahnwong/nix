4d80d2c4fe9d4103572474588cdb488bbc086ef2
Karn Wong <karn@karnwong.me>
2025-09-27 16:07:43 +0700

    gnome: disable clyp shortcut

--- scripts/linux-desktop/04_setup_shortcuts.sh
+++ scripts/linux-desktop/04_setup_shortcuts.sh
@@ -2,6 +2,6 @@
 
 # ------ Flameshot ------
 # <https://flameshot.org/docs/guide/key-bindings/#on-ubuntu-and-other-gnome-based-distros>
-# export via: `dconf dump / | sed -n '/\[org.gnome.settings-daemon.plugins.media-keys/,/^$/p' > custom-shortcuts.conf`
 
+# export via: `dconf dump / | sed -n '/\[org.gnome.settings-daemon.plugins.media-keys/,/^$/p' > custom-shortcuts.conf`
 dconf load / <programs/gnome/custom-shortcuts.conf
