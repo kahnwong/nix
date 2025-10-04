3210bb1d1dab1c87012fa8fd04cf19b436aacc89
Karn Wong <karn@karnwong.me>
2025-10-04 13:48:36 +0700

    zed: add keymap

--- programs/zed/zed.nix
+++ programs/zed/zed.nix
@@ -3,8 +3,11 @@
 {
   home.file = if pkgs.stdenv.isLinux then {
     ".var/app/dev.zed.Zed/config/zed/settings.json".source = ./settings.json;
+    ".var/app/dev.zed.Zed/config/zed/keymap.json".source = ./keymap.json;
   } else {
     ".config/zed/settings.json".source = ./settings.json;
+    ".config/zed/keymap.json".source = ./keymap.json;
+
   };
 
   home.packages = with pkgs; [ ];
