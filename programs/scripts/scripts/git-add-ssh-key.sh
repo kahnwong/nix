a3e4b7e2595494a85463953fdbaa265dac6f3259
Karn Wong <karn@karnwong.me>
2025-10-26 15:53:41 +0700

    script: update ssh-add

--- programs/scripts/scripts/git-add-ssh-key.sh
+++ programs/scripts/scripts/git-add-ssh-key.sh
@@ -4,6 +4,10 @@ if [[ $(uname -s) == 'Darwin' ]]; then
 	ssh-add --apple-use-keychain ~/.ssh/github-signing-key.pem
 
 elif [[ $(uname -s) == 'Linux' ]]; then
+	if [ -z "$SSH_AUTH_SOCK" ]; then
+		eval "$(ssh-agent -s)"
+	fi
+
 	# ssh-agent fish # only required if sometimes ssh version borks
 	ssh-add ~/.ssh/github-signing-key.pem
 fi
