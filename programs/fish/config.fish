6b8c59190a9552d4df05bf3cbfddbbb2045a70be
Karn Wong <karn@karnwong.me>
2025-11-16 14:54:41 +0700

    nix: revert shell.fish

--- programs/fish/config.fish
+++ programs/fish/config.fish
@@ -74,3 +74,49 @@ switch (uname)
     case Linux
         alias zed="/var/lib/flatpak/exports/bin/dev.zed.Zed"
 end
+
+####################
+# CONFIG: SHELL
+####################
+# editor
+set EDITOR nvim
+
+# fzf
+export FZF_DEFAULT_COMMAND='find . \( -name node_modules -o -name .git -o -name cache -o -name dist \) -prune -o -print'
+
+# gpg
+set GPG_TTY tty
+
+# starship
+starship init fish | source
+
+# zoxide
+zoxide init fish | source
+
+# mcfly
+mcfly init fish | source
+set MCFLY_KEY_SCHEME vim
+
+# direnv
+direnv hook fish | source
+
+function ls
+    eza --icons -1 --group-directories-first $argv
+end
+
+function cat
+    bat --theme "Monokai Extended" $argv
+end
+
+function vi
+    nvim $argv
+end
+
+function tere
+    set --local result (command tere $argv)
+    [ -n "$result" ] && cd -- "$result"
+end
+
+function tmux
+    zellij $argv
+end
