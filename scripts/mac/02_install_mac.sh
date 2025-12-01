0495fc9db61a9150149f659141434ffda2fdd589
Karn Wong <karn@karnwong.me>
2025-12-01 11:52:00 +0700

    brew: prune

--- scripts/mac/02_install_mac.sh
+++ scripts/mac/02_install_mac.sh
@@ -13,17 +13,14 @@ brew install mas
 brew install mpv
 
 # cloud storage
-# brew install --cask dropbox
 brew install --cask syncthing
 
 # communication
 # brew install --cask adium
-# brew install --cask whatsapp
+# brew install --cask microsoft-teams
+# brew install --cask slack
 brew install --cask discord
 brew install --cask element
-brew install --cask messenger
-brew install --cask microsoft-teams
-brew install --cask slack
 brew install --cask thunderbird
 brew install --cask zoom
 
@@ -32,9 +29,7 @@ brew install --cask tad
 brew install apache-spark
 
 # dev tools
-# brew install --cask devtoys
 # brew install --cask docker
-# brew install --cask microsoft-remote-desktop
 # brew install --cask mongodb-compass
 brew install --cask fork
 brew install --cask ghostty
@@ -66,9 +61,8 @@ brew install --cask jupyter-notebook-viewer
 brew install --cask qlmarkdown
 
 # productivity
-brew install --cask notion-calendar
-# brew install --cask loom
 brew install --cask macdown
+brew install --cask notion-calendar
 brew install --cask obsidian
 brew install --cask sleek
 brew install --cask todoist
@@ -76,13 +70,7 @@ brew install --cask todotxt
 brew install lihaoyun6/tap/quickrecorder
 
 # utilities
-# brew install --cask hiddenbar
-# brew install --cask kap
-# brew install --cask protonvpn
-# brew install --cask spectacle
-# brew install --cask stats
 # brew install ical-buddy
-# brew install --cask michaelvillar-timer
 brew install --cask alfred
 brew install --cask angry-ip-scanner
 brew install --cask anydesk
@@ -92,23 +80,21 @@ brew install --cask bitwarden
 brew install --cask cheatsheet
 brew install --cask coconutbattery
 brew install --cask flameshot
-brew install --cask hot
 brew install --cask keepingyouawake
 brew install --cask maccy
 brew install --cask mullvadvpn
 brew install --cask raspberry-pi-imager
 brew install --cask rustdesk
-brew install --cask shifty
+brew install --cask spectacle
 brew install --cask the-unarchiver
 brew install --cask tyke
 brew install --cask yubico-authenticator
 brew install --cask yubico-yubikey-manager
-brew install jordanbaird-ice
 
 # peripheral software
 brew install --cask linearmouse
-brew install --cask logitech-camera-settings
-brew install --cask logitech-g-hub
+# brew install --cask logitech-camera-settings
+# brew install --cask logitech-g-hub
 
 echo "================================= Install App Store applications ================================="
 mas install 1176895641 # spark
