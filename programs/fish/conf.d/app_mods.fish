6b0abf619d91ecfcf9c7872945b0ea1453915f6a
Karn Wong <karn@karnwong.me>
2025-12-03 12:14:47 +0700

    mods: add models
    # Conflicts:
    #	programs/fish/conf.d/app_mods.fish

--- programs/fish/conf.d/app_mods.fish
+++ programs/fish/conf.d/app_mods.fish
@@ -1,18 +1,26 @@
 set -gx MODS_GOOGLE_API_TOKEN $(get_fish_secret MODS_GOOGLE_API_TOKEN)
 set -gx MODS_GITHUB_PERSONAL_ACCESS_TOKEN $(get_fish_secret MODS_GITHUB_PERSONAL_ACCESS_TOKEN)
 
-function a
+
+# coding
+function a # ask - coding mode
     mods --no-cache -P $argv
 end
 
-function ap
+function ap # ask - pro (model)
     mods --no-cache -m "gemini-2.5-pro" -P $argv
 end
 
-function as
+# normal
+function as # ask - short answer
     mods --no-cache -R short-answer -P $argv
 end
 
-function an
+function an # ask - normal mode
     mods --no-cache -R default -P $argv
+end
+
+# select
+function aselect # ask - select model
+    mods --no-cache -m "dummy" # -M flat doesn't display all models
 end
