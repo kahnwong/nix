481fab84ad00023fece85d8d3fcd6212957925c0
Karn Wong <karn@karnwong.me>
2025-12-16 20:18:52 +0700

    linux-desktop: disable fuse

--- scripts/linux-desktop/01_setup_linux.sh
+++ scripts/linux-desktop/01_setup_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt install fuse -y # installing this after flatpak seems to bork flatpak, which requires a flatpak reinstall
+# sudo apt install fuse -y # installing this after flatpak seems to bork flatpak, which requires a flatpak reinstall - it can bork gdm
 sudo apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
