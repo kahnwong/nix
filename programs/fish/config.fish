e64fade16c75bcb0c5ccf83cf1e8e3caf40489c6
Karn Wong <karn@karnwong.me>
2025-12-24 12:32:47 +0700

    fish: refactor completions

--- programs/fish/config.fish
+++ programs/fish/config.fish
@@ -136,51 +136,34 @@ end
 ###############################
 # APPS: Completions + Alias
 ###############################
-# pgconn
-if type -p pgconn > /dev/null; pgconn completion fish | source; end
-
-# totp
-if type -p totp > /dev/null; totp completion fish | source; end
-
-# swissknife
-alias sk="swissknife"
-if type -p swissknife > /dev/null; sk completion fish | source; end
-
-# timesheet
-alias ts="timesheet"
-if type -p timesheet > /dev/null; ts completion fish | source; end
-
-# config-init
-alias ci="config-init"
-if type -p config-init > /dev/null; ci completion fish | source; end
-
-# article-summarizer
-alias sm="article-summarizer"
-
-# erp
-if type -p erp > /dev/null; erp completion fish | source; end
-
-# media-discord-rpc
-alias d="media-discord-rpc"
-if type -p media-discord-rpc > /dev/null; d completion fish | source; end
-
-# habit-tracker
-if type -p habit-tracker > /dev/null; habit-tracker completion fish | source; end
-alias h="habit-tracker"
-
-# thailand-train-fare-calculator
-if type -p thailand-train-fare-calculator > /dev/null; thailand-train-fare-calculator completion fish | source; end
-
-# gcal-tui
-alias c="gcal-tui"
-
-# bean-add
-if type -p bean-add > /dev/null; bean-add completion fish | source; end
+# Aliases
 alias ba="bean-add"
-
-# umamit
-if type -p umamit > /dev/null; umamit completion fish | source; end
+alias c="gcal-tui"
+alias ci="config-init"
+alias d="media-discord-rpc"
+alias h="habit-tracker"
+alias sk="swissknife"
+alias sm="article-summarizer"
+alias ts="timesheet"
 alias u="umamit"
 
-# cpubench
-if type -p umamit > /dev/null; cpubench completion fish | source; end
+# Auto-completions
+set -l completion_commands \
+    bean-add \
+    config-init \
+    cpubench \
+    erp \
+    habit-tracker \
+    media-discord-rpc \
+    pgconn \
+    swissknife \
+    thailand-train-fare-calculator \
+    timesheet \
+    totp \
+    umamit
+
+for cmd in $completion_commands
+    if type -p $cmd > /dev/null
+        $cmd completion fish | source
+    end
+end
