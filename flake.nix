58c94d83d7af0b69c6b06ee95bd8d40441a66924
Karn Wong <karn@karnwong.me>
2025-12-24 21:13:42 +0700

    nix: add inu

--- flake.nix
+++ flake.nix
@@ -66,6 +66,18 @@
           }
         ];
       };
+      inu = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          ./hosts/linux/demo/home.nix
+          {
+            home = {
+              username = "kahnwong";
+              homeDirectory = "/home/kahnwong";
+            };
+          }
+        ];
+      };
       server = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [
