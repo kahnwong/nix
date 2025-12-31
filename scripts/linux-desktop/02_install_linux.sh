15ec8e1a539247891d69ab733a7cf7e472c1744b
Karn Wong <karn@karnwong.me>
2025-12-31 12:00:18 +0700

    linux-desktop: add gparted

--- scripts/linux-desktop/02_install_linux.sh
+++ scripts/linux-desktop/02_install_linux.sh
@@ -1,60 +1,63 @@
 #!/bin/bash
 
 ### applications
-sudo apt install dolphin -y
-sudo apt install file-roller -y
-sudo apt install konsole -y
-sudo apt install xclip -y
+sudo apt install -y \
+	dolphin \
+	file-roller \
+	gparted \
+	konsole \
+	xclip
 sudo snap install mpv
 
-# flatpak install flathub com.github.hluk.copyq -y
-# flatpak install flathub io.kinvolk.Headlamp -y
-# flatpak install flathub io.mpv.Mpv -y
-flatpak install flathub app.freelens.Freelens -y
-flatpak install flathub com.bitwarden.desktop -y
-flatpak install flathub com.calibre_ebook.calibre -y
-flatpak install flathub com.dec05eba.gpu_screen_recorder -y
-flatpak install flathub com.discordapp.Discord -y
-flatpak install flathub com.dropbox.Client -y
-flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y
-flatpak install flathub com.github.PintaProject.Pinta -y
-flatpak install flathub com.github.iwalton3.jellyfin-media-player -y
-flatpak install flathub com.github.jeromerobert.pdfarranger -y
-flatpak install flathub com.github.johnfactotum.Foliate -y
-flatpak install flathub com.github.qarmin.czkawka -y
-flatpak install flathub com.github.ransome1.sleek -y
-flatpak install flathub com.github.tchx84.Flatseal -y
-flatpak install flathub com.github.zocker_160.SyncThingy -y
-flatpak install flathub com.google.Chrome -y
-flatpak install flathub com.heroicgameslauncher.hgl -y
-flatpak install flathub com.obsproject.Studio -y
-flatpak install flathub com.obsproject.Studio.Plugin.BackgroundRemoval -y
-flatpak install flathub com.rustdesk.RustDesk -y
-flatpak install flathub com.slack.Slack -y
-flatpak install flathub com.todoist.Todoist -y
-flatpak install flathub com.transmissionbt.Transmission -y
-flatpak install flathub dev.zed.Zed -y
-flatpak install flathub io.github.TransmissionRemoteGtk -y
-flatpak install flathub io.github.ciromattia.kcc -y
-flatpak install flathub io.github.dweymouth.supersonic -y
-flatpak install flathub io.github.flattool.Ignition -y
-flatpak install flathub io.github.flattool.Warehouse -y
-flatpak install flathub io.github.josephmawa.Bella -y
-flatpak install flathub io.github.sigmasd.stimulator -y
-flatpak install flathub io.github.vemonet.EmojiMart -y
-flatpak install flathub io.gitlab.adhami3310.Impression -y
-flatpak install flathub io.gitlab.librewolf-community -y
-flatpak install flathub md.obsidian.Obsidian -y
-flatpak install flathub net.puddletag.puddletag -y
-flatpak install flathub org.audacityteam.Audacity -y
-flatpak install flathub org.fkoehler.KTailctl -y
-flatpak install flathub org.flameshot.Flameshot -y
-flatpak install flathub org.freefilesync.FreeFileSync -y
-flatpak install flathub org.gnome.Fractal -y
-flatpak install flathub org.gnome.gitlab.cheywood.Buffer -y
-flatpak install flathub org.gpodder.gpodder -y
-flatpak install flathub org.kde.okular -y
-flatpak install flathub org.mozilla.Thunderbird -y
-flatpak install flathub org.raspberrypi.rpi-imager -y
-flatpak install flathub org.telegram.desktop -y
-flatpak install flathub us.zoom.Zoom -y
+#com.dropbox.Client
+#com.github.hluk.copyq
+#io.kinvolk.Headlamp
+#io.mpv.Mpv
+flatpak install -y flathub \
+	app.freelens.Freelens \
+	com.bitwarden.desktop \
+	com.calibre_ebook.calibre \
+	com.dec05eba.gpu_screen_recorder \
+	com.discordapp.Discord \
+	com.github.IsmaelMartinez.teams_for_linux \
+	com.github.PintaProject.Pinta \
+	com.github.iwalton3.jellyfin-media-player \
+	com.github.jeromerobert.pdfarranger \
+	com.github.johnfactotum.Foliate \
+	com.github.qarmin.czkawka \
+	com.github.ransome1.sleek \
+	com.github.tchx84.Flatseal \
+	com.github.zocker_160.SyncThingy \
+	com.google.Chrome \
+	com.heroicgameslauncher.hgl \
+	com.obsproject.Studio \
+	com.obsproject.Studio.Plugin.BackgroundRemoval \
+	com.rustdesk.RustDesk \
+	com.slack.Slack \
+	com.todoist.Todoist \
+	com.transmissionbt.Transmission \
+	dev.zed.Zed \
+	io.github.TransmissionRemoteGtk \
+	io.github.ciromattia.kcc \
+	io.github.dweymouth.supersonic \
+	io.github.flattool.Ignition \
+	io.github.flattool.Warehouse \
+	io.github.josephmawa.Bella \
+	io.github.sigmasd.stimulator \
+	io.github.vemonet.EmojiMart \
+	io.gitlab.adhami3310.Impression \
+	io.gitlab.librewolf-community \
+	md.obsidian.Obsidian \
+	net.puddletag.puddletag \
+	org.audacityteam.Audacity \
+	org.fkoehler.KTailctl \
+	org.flameshot.Flameshot \
+	org.freefilesync.FreeFileSync \
+	org.gnome.Fractal \
+	org.gnome.gitlab.cheywood.Buffer \
+	org.gpodder.gpodder \
+	org.kde.okular \
+	org.mozilla.Thunderbird \
+	org.raspberrypi.rpi-imager \
+	org.telegram.desktop \
+	us.zoom.Zoom
