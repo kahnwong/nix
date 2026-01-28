8739edd56cd6d80c94bc40ce7df7c5429cabe670
Karn Wong <karn@karnwong.me>
2026-01-28 14:39:49 +0700

    fish: remove flameshot kill

--- programs/scripts/scripts.nix
+++ programs/scripts/scripts.nix
@@ -1,10 +1,14 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   home.file."scripts/,code-to-image.sh".source = ./scripts/code-to-image.sh;
   home.file."scripts/,convert-to-webp.sh".source = ./scripts/convert-to-webp.sh;
   home.file."scripts/,f2-rename-music.sh".source = ./scripts/f2-rename-music.sh;
-  home.file."scripts/,flameshot-kill.sh".source = ./scripts/flameshot-kill.sh;
   home.file."scripts/,gh-archive-repo.sh".source = ./scripts/gh-archive-repo.sh;
   home.file."scripts/,gh-list-pr.sh".source = ./scripts/gh-list-pr.sh;
   home.file."scripts/,gh-list-repos.sh".source = ./scripts/gh-list-repos.sh;
--- programs/scripts/scripts/flameshot-kill.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pgrep flameshot | xargs kill
