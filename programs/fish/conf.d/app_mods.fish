215431e87f6ff04c7d4aeb414a63c19ddeb2a17e
Karn Wong <karn@karnwong.me>
2026-01-29 20:18:33 +0700

    mods: update model

--- programs/fish/conf.d/app_mods.fish
+++ programs/fish/conf.d/app_mods.fish
@@ -1,5 +1,6 @@
 set -gx MODS_GOOGLE_API_TOKEN $(get_fish_secret MODS_GOOGLE_API_TOKEN)
-set -gx MODS_GITHUB_PERSONAL_ACCESS_TOKEN $(get_fish_secret MODS_GITHUB_PERSONAL_ACCESS_TOKEN)
+set -gx MODS_CUSTOM_API_TOKEN $(get_fish_secret AI_GATEWAY_API_KEY)
+# set -gx MODS_GITHUB_PERSONAL_ACCESS_TOKEN $(get_fish_secret MODS_GITHUB_PERSONAL_ACCESS_TOKEN)
 
 
 # coding
@@ -7,9 +8,9 @@ function a # ask - coding mode
     mods --no-cache -P $argv
 end
 
-function ap # ask - pro (model)
-    mods --no-cache -m "gemini-2.5-pro" -P $argv
-end
+# function ap # ask - pro (model)
+#     mods --no-cache -m "gemini-2.5-pro" -P $argv
+# end
 
 # normal
 function as # ask - short answer
@@ -20,7 +21,7 @@ function an # ask - normal mode
     mods --no-cache -R default -P $argv
 end
 
-# select
-function aselect # ask - select model
-    mods --no-cache -m "dummy" # -M flat doesn't display all models
-end
+# # select
+# function aselect # ask - select model
+#     mods --no-cache -m "dummy" # -M flat doesn't display all models
+# end
