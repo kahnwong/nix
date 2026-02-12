921d72ff4a3389ed4df804b6e760144d4a0b012d
Karn Wong <karn@karnwong.me>
2026-02-12 18:55:29 +0700

    linux-desktop: add languages setup

--- scripts/linux-desktop/04_setup_shortcuts.sh
+++ scripts/linux-desktop/04_setup_shortcuts.sh
@@ -10,3 +10,10 @@ dconf load / <programs/gnome/custom-shortcuts.conf
 # ------ tiling assistant shortcuts ------
 # export via: `dconf dump / | sed -n '/\[org.gnome.shell.extensions.tiling-assistant/,/^$/p' > programs/gnome/tiling-assistant-shortcuts.conf`
 dconf load / <programs/gnome/tiling-assistant-shortcuts.conf
+
+# ------ languages ------
+# export via: `dconf dump / | sed -n '/\[org.gnome.desktop.wm.keybindings/,/^$/p' > programs/gnome/wm-keybindings.conf`
+dconf load / <programs/gnome/wm-keybindings.conf
+
+# export via: `dconf dump / | sed -n '/\[org.gnome.desktop.input-sources/,/^$/p' > programs/gnome/input-sources.conf`
+dconf load / <programs/gnome/input-sources.conf
