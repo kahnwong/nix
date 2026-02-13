cf1f8a2e114d091daaca6b59348a7da22620f79a
Karn Wong <karn@karnwong.me>
2026-02-13 18:52:47 +0700

    common-dev: add terraform

--- scripts/common-dev/01_setup.sh
+++ scripts/common-dev/01_setup.sh
@@ -9,4 +9,9 @@ if [[ $(uname -s) == 'Linux' ]]; then
 		libncursesw5-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libxslt1-dev \
 		llvm make tk-dev xz-utils zlib1g-dev gcc-aarch64-linux-gnu \
 		-y
+
+	# terraform
+	wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+	sudo apt update && sudo apt install terraform
 fi
