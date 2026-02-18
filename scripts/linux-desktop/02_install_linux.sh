7b3baff167e9a1368c7b61aa0708f82cd96f599e
Karn Wong <karn@karnwong.me>
2026-02-18 01:31:00 -0500

    flatpak: add steam

--- scripts/linux-desktop/02_install_linux.sh
+++ scripts/linux-desktop/02_install_linux.sh
@@ -2,11 +2,11 @@
 
 ### applications
 sudo apt install -y \
-	dolphin \
-	file-roller \
-	gparted \
-	konsole \
-	xclip
+  dolphin \
+  file-roller \
+  gparted \
+  konsole \
+  xclip
 sudo snap install mpv
 
 #com.dropbox.Client
@@ -14,54 +14,60 @@ sudo snap install mpv
 #io.kinvolk.Headlamp
 #io.mpv.Mpv
 flatpak install -y flathub \
-	app.freelens.Freelens \
-	app.zen_browser.zen \
-	com.bilingify.readest \
-	com.bitwarden.desktop \
-	com.calibre_ebook.calibre \
-	com.dec05eba.gpu_screen_recorder \
-	com.discordapp.Discord \
-	com.github.IsmaelMartinez.teams_for_linux \
-	com.github.PintaProject.Pinta \
-	com.github.iwalton3.jellyfin-media-player \
-	com.github.jeromerobert.pdfarranger \
-	com.github.qarmin.czkawka \
-	com.github.ransome1.sleek \
-	com.github.tchx84.Flatseal \
-	com.github.zocker_160.SyncThingy \
-	com.google.Chrome \
-	com.heroicgameslauncher.hgl \
-	com.obsproject.Studio \
-	com.obsproject.Studio.Plugin.BackgroundRemoval \
-	com.rustdesk.RustDesk \
-	com.slack.Slack \
-	com.todoist.Todoist \
-	com.transmissionbt.Transmission \
-	dev.zed.Zed \
-	io.github.TransmissionRemoteGtk \
-	io.github.ciromattia.kcc \
-	io.github.dweymouth.supersonic \
-	io.github.flattool.Ignition \
-	io.github.flattool.Warehouse \
-	io.github.josephmawa.Bella \
-	io.github.sigmasd.stimulator \
-	io.github.vemonet.EmojiMart \
-	io.gitlab.adhami3310.Impression \
-	io.gitlab.librewolf-community \
-	md.obsidian.Obsidian \
-	net.puddletag.puddletag \
-	org.audacityteam.Audacity \
-	org.fkoehler.KTailctl \
-	org.flameshot.Flameshot \
-	org.freefilesync.FreeFileSync \
-	org.gnome.Fractal \
-	org.gnome.gitlab.cheywood.Buffer \
-	org.gpodder.gpodder \
-	org.kde.okular \
-	org.mozilla.Thunderbird \
-	org.raspberrypi.rpi-imager \
-	org.telegram.desktop \
-	us.zoom.Zoom
+  app.freelens.Freelens \
+  app.zen_browser.zen \
+  com.bilingify.readest \
+  com.bitwarden.desktop \
+  com.calibre_ebook.calibre \
+  com.dec05eba.gpu_screen_recorder \
+  com.discordapp.Discord \
+  com.github.IsmaelMartinez.teams_for_linux \
+  com.github.PintaProject.Pinta \
+  com.github.iwalton3.jellyfin-media-player \
+  com.github.jeromerobert.pdfarranger \
+  com.github.qarmin.czkawka \
+  com.github.ransome1.sleek \
+  com.github.tchx84.Flatseal \
+  com.github.zocker_160.SyncThingy \
+  com.google.Chrome \
+  com.obsproject.Studio \
+  com.obsproject.Studio.Plugin.BackgroundRemoval \
+  com.rustdesk.RustDesk \
+  com.slack.Slack \
+  com.todoist.Todoist \
+  com.transmissionbt.Transmission \
+  dev.zed.Zed \
+  io.github.TransmissionRemoteGtk \
+  io.github.ciromattia.kcc \
+  io.github.dweymouth.supersonic \
+  io.github.flattool.Ignition \
+  io.github.flattool.Warehouse \
+  io.github.josephmawa.Bella \
+  io.github.sigmasd.stimulator \
+  io.github.vemonet.EmojiMart \
+  io.gitlab.adhami3310.Impression \
+  net.puddletag.puddletag \
+  org.audacityteam.Audacity \
+  org.fkoehler.KTailctl \
+  org.flameshot.Flameshot \
+  org.freefilesync.FreeFileSync \
+  org.gnome.Fractal \
+  org.gnome.gitlab.cheywood.Buffer \
+  org.gpodder.gpodder \
+  org.kde.okular \
+  org.mozilla.Thunderbird \
+  org.raspberrypi.rpi-imager \
+  org.telegram.desktop \
+  us.zoom.Zoom
 
 # set flameshot permission
 flatpak permission-set screenshot screenshot org.flameshot.Flameshot yes
+
+# sailfish only
+if [[ $(uname -n) == 'sailfish' ]]; then
+  flatpak install -y flathub \
+    com.heroicgameslauncher.hgl \
+    com.valvesoftware.Steam
+
+  flatpak override --user --device=dri --socket=x11 --socket=wayland com.valvesoftware.Steam
+fi
