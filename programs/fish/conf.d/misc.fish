1615780a8d70cc43a8a0d554ef0adfb214fe2730
Karn Wong <karn@karnwong.me>
2026-02-27 17:48:15 +0700

    repo-switcher: add open in ide

--- programs/fish/conf.d/misc.fish
+++ programs/fish/conf.d/misc.fish
@@ -78,7 +78,14 @@ function llm
 end
 
 # repo-switcher
-function r
+function r # open in IDE
+    set path (command repo-switcher $argv)
+    if test $status -eq 0
+        idea $path &
+    end
+end
+
+function rcd # cd into directory
     set path (command repo-switcher $argv)
     if test $status -eq 0
         cd $path
--- programs/fish/config.fish
+++ programs/fish/config.fish
@@ -166,6 +166,7 @@ end
 if type -q repo-switcher
     repo-switcher completion fish | source
 
-    # wrap the completions so 'r' behaves like 'repo-switcher'
+    # wrap the completions so 'r' & 'rcd' behaves like 'repo-switcher'
     complete -c r -w repo-switcher
+    complete -c rcd -w repo-switcher
 end
