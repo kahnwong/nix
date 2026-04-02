8193d5c3c23f68fad689e413066ffa47e731eb89
Karn Wong <karn@karnwong.me>
2026-04-02 16:14:15 +0700

    ghostty: update ppa

--- scripts/linux-desktop/install_ghostty.sh
+++ scripts/linux-desktop/install_ghostty.sh
@@ -2,16 +2,9 @@
 
 # <https://github.com/mkasberg/ghostty-ubuntu>
 
-# shellcheck disable=SC1091
-source /etc/os-release
-GHOSTTY_DEB_URL=$(
-	curl -s https://api.github.com/repos/mkasberg/ghostty-ubuntu/releases/latest |
-		grep -oP "https://github.com/mkasberg/ghostty-ubuntu/releases/download/[^\s/]+/ghostty_[^\s/_]+_amd64_${VERSION_ID}.deb"
-)
-GHOSTTY_DEB_FILE=$(basename "$GHOSTTY_DEB_URL")
-curl -LO "$GHOSTTY_DEB_URL"
-sudo dpkg -i "$GHOSTTY_DEB_FILE"
-rm "$GHOSTTY_DEB_FILE"
+sudo add-apt-repository ppa:mkasberg/ghostty-ubuntu
+sudo apt update
+sudo apt install ghostty
 
 # disable welcome message for fish shell on mac
 # <https://stackoverflow.com/questions/15769615/remove-last-login-message-for-new-tabs-in-terminal>
