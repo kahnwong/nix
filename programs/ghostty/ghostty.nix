536fc0585a6f9d477646b18a48144443ac145f69
Karn Wong <karn@karnwong.me>
2026-04-10 12:38:30 +0700

    nix: remove impure flag

--- programs/ghostty/ghostty.nix
+++ programs/ghostty/ghostty.nix
@@ -1,19 +1,34 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
   home.file = {
     ".config/ghostty/config" = {
-      text = with pkgs;
+      text =
+        with pkgs;
         let
-          mac = builtins.replaceStrings [ "FISH_PATH" "FONT_SIZE" ] [
-            "/Users/${builtins.getEnv "USER"}/.nix-profile/bin/fish"
-            "13.5"
-          ] (builtins.readFile ./config);
-          linux = builtins.replaceStrings [ "FISH_PATH" "FONT_SIZE" ] [
-            "/home/${builtins.getEnv "USER"}/.nix-profile/bin/fish"
-            "10"
-          ] (builtins.readFile ./config);
-        in (if stdenv.isLinux then linux else mac);
+          mac =
+            builtins.replaceStrings
+              [ "FISH_PATH" "FONT_SIZE" ]
+              [
+                "/Users/${config.home.username}/.nix-profile/bin/fish"
+                "13.5"
+              ]
+              (builtins.readFile ./config);
+          linux =
+            builtins.replaceStrings
+              [ "FISH_PATH" "FONT_SIZE" ]
+              [
+                "/home/${config.home.username}/.nix-profile/bin/fish"
+                "10"
+              ]
+              (builtins.readFile ./config);
+        in
+        (if stdenv.isLinux then linux else mac);
     };
   };
 }
