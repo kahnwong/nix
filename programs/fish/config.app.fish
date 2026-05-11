7eef40851fcdacca6702d71cc63a9104b201c70f
Karn Wong <karn@karnwong.me>
2026-05-11 13:19:04 +0700

    fish: add app profile

---
+++ programs/fish/config.app.fish
@@ -0,0 +1,110 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting
+
+set EDITOR nvim
+
+zoxide init fish | source
+
+alias ba="bean-add"
+alias h="habit-tracker"
+alias sk="swissknife"
+alias sm="article-summarizer"
+alias ts="timesheet"
+alias u="umamit"
+alias vi="nvim"
+
+# Auto-completions
+set -l completion_commands \
+    bean-add \
+    erp \
+    habit-tracker \
+    swissknife \
+    timesheet \
+    umamit
+
+for cmd in $completion_commands
+    if command -v $cmd > /dev/null
+        $cmd completion fish | source
+    end
+end
+
+function ht
+    habit-tracker today
+end
+
+function hw
+    habit-tracker week
+end
+
+# base
+function t
+    topydo $argv
+end
+
+function tc
+    topydo columns
+end
+
+function tsort
+    topydo sort asc:context asc:project
+end
+
+# query
+function tt
+    topydo ls -s desc:priority,asc:context,asc:project due:"<=today"
+end
+
+function tmr
+    topydo due:tomorrow
+end
+
+function tlh
+    # lh = look ahead
+    topydo due:"<=7d"
+end
+
+function tr
+    topydo ls -s asc:context -- -@work -@recurring -@tinkering -@meet
+end
+
+function tw
+    topydo @work
+end
+
+function tkr
+    topydo ls -s desc:priority,asc:project @tinkering
+end
+
+# add
+function tat
+    topydo add due:today $argv
+end
+
+function taw
+    topydo add @work $argv
+end
+
+function ut
+    umamit today
+end
+
+function uw
+    umamit week
+end
+
+function um
+    umamit month
+end
+
+function uy
+    umamit yesterday
+end
+
+function sts
+    curl "http://127.0.0.1:8384/rest/db/scan?folder=$SYNCTHING_FOLDER_ID" \
+        -X POST \
+        -H "X-API-Key: xxxxxxx"
+end
--- programs/fish/fish-generic.nix
+++ programs/fish/fish-generic.nix
@@ -10,7 +10,7 @@
     ".config/fish/conf.d/devops_system.fish".source = ./conf.d/devops_system.fish;
     ".config/fish/conf.d/git.fish".source = ./conf.d/git.fish;
     ".config/fish/conf.d/misc.fish".source = ./conf.d/misc.fish;
-    ".config/fish/config.fish".source = ./config-generic.fish;
+    ".config/fish/config.fish".source = ./config.generic.fish;
   };
 
   home.packages = with pkgs; [
