01ddb90bae28252dbf7152d35b3bbbac67387157
Karn Wong <karn@karnwong.me>
2026-05-20 22:36:20 +0700

    pkgs: replace gemini-cli with antigravity

--- scripts/common-dev/03-install-ai-tools.sh
+++ scripts/common-dev/03-install-ai-tools.sh
@@ -4,7 +4,7 @@ curl -fsSL https://claude.ai/install.sh | bash
 curl -fsSL https://cli.kiro.dev/install | bash
 #curl -fsSL https://gh.io/copilot-install | bash
 ubi --project charmbracelet/crush --in ~/.local/bin
-yarn global add @google/gemini-cli
+curl -fsSL https://antigravity.google/cli/install.sh | bash
 yarn global add opencode-ai
 
 # git-ai
