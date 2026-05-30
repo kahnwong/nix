0491f37683e6d05fe40f84cc1720a37e85965061
Karn Wong <karn@karnwong.me>
2026-05-30 16:34:22 +0700

    nix: update pkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration and home-manager configurations for mac and debian gnu/linux";
 
   nixConfig = {
-    extra-trusted-substituters = [ "https://cache.flox.dev" ];
+    extra-substituters = [ "https://cache.flox.dev" ];
     extra-trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
   };
 
