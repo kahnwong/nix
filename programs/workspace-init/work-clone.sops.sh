bb06a839d599588aa56ff08306c30f96536c6195
Karn Wong <karn@karnwong.me>
2026-06-06 01:32:28 +0700

    workspace-init: add repo

--- programs/workspace-init/work-clone.sops.sh
+++ programs/workspace-init/work-clone.sops.sh
@@ -1,15 +1,15 @@
 {
-	"data": "ENC[AES256_GCM,data:rYovgXcPaQgU/kQy3shcihSBB5TgNyt2+jL7b1AZrr5h+OnkxHsRGrudD4cIKRSft/okfAkuaEB07XUxayRUG5ZTtYlQmRB55aXPz1pcfO/XUYWHkZDpTRK3E0pBqrulJ4PPWb5bYPc/tw99F4iHAq2OA8sHneqQwA3SLeRant6Kju0+88+LcctWGEEHa1uRlYo+MCktcUM+Y9ZLRJiFzRGghZS/PzaA4U10wj7NEJZLsm5+X145hOSPIG6fmb0=,iv:5xKEzV6ySAuxlhFg+0qzp+1TFSrn89VI5DokbqJLPUo=,tag:epO9lgPU6/HCsTw+Y+DXjA==,type:str]",
+	"data": "ENC[AES256_GCM,data:PIU06vLaaPkxm/DsIIlDyKaNHpy/q2x0VP222vjPmOHlPfXvj6ucu3qEO/KmK2UmKkKokZ7YxlJA1KMPD6gOX/ETy0AcsLiQNFSnNKYp/f8taoS1QbboJ106wNjV82rFVuWGLULvgJxHp+g2PF9wf0yNeJXwK3zuiENjEHJ+xPHe9oc0b3l535237MTs0WaPoYFA+MR6EWVPofFwyU9kLP9QIXfy4PzNQ+4ve8i4n5B+CG8BsrTiI1AiwNv78ZQKd4tP6qhzOIOLDkxNiwXJxf9SPiJY5CaNd2B6mnRp5bzHGNK1BA57BrzKxh9RW98dIHfs0Hc=,iv:IQ1UBu5Ddp1gx9t4vWf63zE6sKfGqER8KljTUUv+iPg=,tag:LHJ8eMVKPe0qxDEZRLUYGA==,type:str]",
 	"sops": {
 		"age": [
 			{
-				"recipient": "age1m954z8ajdedsmvr9u89ud5v2zednmxgvvzuahpfy5fcdp97yn99ssy2wmm",
-				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBvU25YU2hnbTk5eFZoc0l4\nV09qL1VkQ3JRU0g2ZjhyaXhYOTJtaXVXQ0JVCkZBbHZLUHI2Z1NZUUlhM3crSlJU\neEFzNGJZTFU2c1BFMzFUa2hORHV5YncKLS0tIG9sVlN5RUR5UEtWaUV0OXRWa0h5\nMFFLQTA3QklhSTNVQTFDYXJuREs2bkUKPICvQVadd5L94bvgvDta73UakdniZLoV\nxbGfUt3iFo42Mmj8uqcpb3CkovEomWblEnFtYesYo6G1AaMs5hXuPg==\n-----END AGE ENCRYPTED FILE-----\n"
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBvU25YU2hnbTk5eFZoc0l4\nV09qL1VkQ3JRU0g2ZjhyaXhYOTJtaXVXQ0JVCkZBbHZLUHI2Z1NZUUlhM3crSlJU\neEFzNGJZTFU2c1BFMzFUa2hORHV5YncKLS0tIG9sVlN5RUR5UEtWaUV0OXRWa0h5\nMFFLQTA3QklhSTNVQTFDYXJuREs2bkUKPICvQVadd5L94bvgvDta73UakdniZLoV\nxbGfUt3iFo42Mmj8uqcpb3CkovEomWblEnFtYesYo6G1AaMs5hXuPg==\n-----END AGE ENCRYPTED FILE-----\n",
+				"recipient": "age1m954z8ajdedsmvr9u89ud5v2zednmxgvvzuahpfy5fcdp97yn99ssy2wmm"
 			}
 		],
-		"lastmodified": "2026-05-16T05:49:59Z",
-		"mac": "ENC[AES256_GCM,data:znyE1AelHDLYG99S3vkTvCsB19k/4ASHyGOxdJ2Nac5soWprkdYYS12/2edAg1SXKOZf6HLG6rrNnVPT84u9u5J99N2CdRJbOen/23oNad/Do8EeJnaeVb5Z7PW41AjXl0rqLX/12Qt6qFvUK5VU3Z54WtVWMuLZN3GSY2M0Zg0=,iv:C0eMgqQUvTIGBxhmTuUCJNqdq7TEyp6XFyhOIft+r3A=,tag:oJxyyoekgx0p0M5xCGkR8g==,type:str]",
+		"lastmodified": "2026-06-05T18:32:20Z",
+		"mac": "ENC[AES256_GCM,data:Yd1fWvC4cd1VCI1mJbJzSO8gOhgmQlMKoGsq5hyzJXHlL9UKyL/dK9HQVWjHdMKN7kXo8byx+4eYkXJ3ar7roFXAm+s3WDAWYEjX9SFHmt2+jXiPk9TnkjfjuBguv1VX4C53maEfALwiFSbofx2k1a4JXdh9S+jgQBS753Y1XrE=,iv:QcQ3VG0c5ypS7+mz/VdCPpYIY9Ntm0Jn8JP0B/Hwmyw=,tag:eTN8b5+mftdGAWUFVt3cgg==,type:str]",
 		"unencrypted_suffix": "_unencrypted",
-		"version": "3.12.2"
+		"version": "3.13.1"
 	}
 }
