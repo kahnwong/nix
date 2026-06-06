b78bbe7224dfdf69c5cc5bb0933ae8c0b580a298
Karn Wong <karn@karnwong.me>
2026-06-06 19:56:39 +0700

    tlp: use ppa

--- scripts/linux/install-tlp.sh
+++ scripts/linux/install-tlp.sh
@@ -4,12 +4,16 @@ if [ -f /etc/os-release ]; then
 	source /etc/os-release
 
 	if [ "$ID" = "ubuntu" ]; then
-		sudo apt install tlp cpufrequtils -y
+		sudo add-apt-repository ppa:linrunner/tlp
+		sudo apt update
+
+		sudo apt install cpufrequtils tlp tlp-pd tlp-rdw -y
 
 		if [[ $(uname -n) == 'redfin' ]]; then
 			sudo cp ./programs/tlp/tlp.redfin.conf /etc/tlp.conf
 		fi
 
+		sudo systemctl enable --now tlp-pd.service
 		sudo tlp start
 	fi
 fi
