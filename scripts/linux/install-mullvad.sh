66bbf448140c9b2dfa0d38a71a1efb4117656e78
Karn Wong <karn@karnwong.me>
2026-06-29 11:03:53 +0700

    linux: add mullvad for fedora

--- scripts/linux/install-mullvad.sh
+++ scripts/linux/install-mullvad.sh
@@ -2,12 +2,19 @@
 
 # <https://mullvad.net/en/download/vpn/linux>
 
-# Download the Mullvad signing key
-sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+if [ -f /etc/os-release ]; then
+	source /etc/os-release
 
-# Add the Mullvad repository server to apt
-echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$(dpkg --print-architecture)] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+	if [ "$ID" = "ubuntu" ]; then
+		sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
+		echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$(dpkg --print-architecture)] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
 
-# Install the package
-sudo apt update
-sudo apt install mullvad-vpn
+		sudo apt update
+		sudo apt install mullvad-vpn
+	elif [ "$ID" = "fedora-asahi-remix" ]; then
+		sudo dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo
+		sudo dnf config-manager --add-repo https://repository.mullvad.net/rpm/stable/mullvad.repo
+
+		sudo dnf install mullvad-vpn
+	fi
+fi
