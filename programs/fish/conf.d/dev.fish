7e121772ba99b807e2721364c8f1aca678c4b154
Karn Wong <karn@karnwong.me>
2026-07-11 14:35:35 +0700

    repo-switcher: merge rcd into r

--- programs/fish/conf.d/dev.fish
+++ programs/fish/conf.d/dev.fish
@@ -2,17 +2,12 @@
 function r
     set path (command repo-switcher $argv)
     if test $status -eq 0
-        if hostname != "steelhead"
+        if test "$hostname" != "steelhead"
             idea $path &
         else
             zed $path
         end
-    end
-end
 
-function rcd
-    set path (command repo-switcher $argv)
-    if test $status -eq 0
         cd $path
     end
 end
