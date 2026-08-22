2b135b21069d5874f46b9b95170f7c4f016ea373
Karn Wong <karn@karnwong.me>
2026-08-22 19:18:30 +0700

    repo-switcher: add ide confirmation

--- programs/fish/conf.d/dev.fish
+++ programs/fish/conf.d/dev.fish
@@ -2,10 +2,13 @@
 function r
     set path (command repo-switcher $argv)
     if test $status -eq 0
-        if test "$hostname" != "steelhead"
-            idea $path &
-        else
-            zed $path
+        read -l -P "Launch IDE? [y/N] " launch_ide
+        if string match -qir '^(y|yes)$' -- "$launch_ide"
+            if test "$hostname" != "steelhead"
+                idea $path &
+            else
+                zed $path
+            end
         end
 
         cd $path
