aa98460b26cb345979eae76a0823341597d00061
Karn Wong <karn@karnwong.me>
2026-09-02 21:38:47 +0700

    scripts: remove unused

--- programs/scripts/scripts.nix
+++ programs/scripts/scripts.nix
@@ -6,7 +6,6 @@ _:
     "scripts/,convert-to-webp.sh".source = ./convert-to-webp.sh;
     "scripts/,f2-rename-music.sh".source = ./f2-rename-music.sh;
     "scripts/,gh-archive-repo.sh".source = ./gh-archive-repo.sh;
-    "scripts/,gh-list-pr.sh".source = ./gh-list-pr.sh;
     "scripts/,gh-list-repos.sh".source = ./gh-list-repos.sh;
     "scripts/,zip-compress.sh".source = ./zip-compress.sh;
   };
