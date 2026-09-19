c6fb57affa9823017941f023ae3f4ea5b4511556
Karn Wong <karn@karnwong.me>
2026-09-19 10:57:46 +0700

    nix: ignore error if ubi package doesn't exist on target platform

--- hosts/pkgs-ubi.nix
+++ hosts/pkgs-ubi.nix
@@ -41,7 +41,19 @@ let
         | xargs -n 1 -P "$UBI_INSTALL_JOBS" ${pkgs.bash}/bin/bash -c '
             project="$1"
             echo "--> Processing $project..."
-            /usr/local/bin/ubi --project "$project" --in "$UBI_INSTALL_DIR"
+            if ! output=$(/usr/local/bin/ubi --project "$project" --in "$UBI_INSTALL_DIR" 2>&1); then
+              printf "%s\n" "$output"
+              case "$output" in
+                *"could not find a release asset for this OS"*)
+                  echo "--> Skipping $project: no release asset for this platform."
+                  ;;
+                *)
+                  exit 1
+                  ;;
+              esac
+            elif [ -n "$output" ]; then
+              printf "%s\n" "$output"
+            fi
           ' _
 
       echo "--> Done!"
